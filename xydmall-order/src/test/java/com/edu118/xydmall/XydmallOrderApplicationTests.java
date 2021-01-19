1685998ba11faca9bdbbbcf41bbdda57c9cd59cc
ajavaboy666 <75707491+ajavaboy666@users.noreply.github.com>
2021-01-19 17:07:58 +0800

    商城初始化

---
+++ xydmall-order/src/test/java/com/edu118/xydmall/XydmallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu118.xydmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XydmallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ xydmall-warehouse/src/test/java/com/edu118/xydmall/XydmallWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu118.xydmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XydmallWarehouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
